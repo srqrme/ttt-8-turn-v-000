746b93d7f602784acbf5fb8e21a2710b8ddd2a0b
Sharmaine Requerme <srqrme@gmail.com>
2018-05-29 16:41:42 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,11 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  !position_taken?(board, index) && index.between?(0, 8)
+  if position_taken?(board, index) == false && (index).between? (0, 8) == true
+    true
+  else
+    false
+  end
 end
 
 def input_to_index(input)
